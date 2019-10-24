76adcbc4d130d353a7cd12338c20e9692a0058d8
Md. Al Amin <alamin.ece.ku@gmail.com>
2019-10-24 03:29:22 -0700

    settings update

--- stoolish/settings.py
+++ stoolish/settings.py
@@ -116,13 +116,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-)
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
