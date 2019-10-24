4a2b1ca623423d4b6b04d0c87a1897b30b7e7b1e
Md. Al Amin <alamin.ece.ku@gmail.com>
2019-10-24 01:11:38 -0700

    Style changes here and there

--- stool/urls.py
+++ stool/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view()),
     path('profile/', views.profile),
     path('', views.home),
     path('logout/', views.logout_view),
     path('signup/', views.sign_up),
-    path('<username>/', views.post_on_wall),
+    path('<username>/', views.post_on_wall, name="post_on_wall"),
 ]
